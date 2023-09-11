e5689f660f70c52c1b16bdcc5becc73e22e97e93
Usama Rabie <76918546+UsamaRabie@users.noreply.github.com>
2023-09-12 01:38:11 +0300

    Update Contacts.jsx

--- src/page/contacts/Contacts.jsx
+++ src/page/contacts/Contacts.jsx
@@ -7,10 +7,7 @@ import Header from "../../components/Header";
 const Contacts = () => {
   return (
     <Box>
-      <Header
-        title="CONTACTS"
-        subTitle="List of Contacts for Future Reference"
-      />
+     
 
       <Box sx={{ height: 650, width: "99%", mx: "auto" }}>
         <DataGrid

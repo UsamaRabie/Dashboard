a2a5003a849279000ed8b3aa784ff0cbad4e93d3
Usama Rabie <76918546+UsamaRabie@users.noreply.github.com>
2023-09-12 01:37:48 +0300

    Update Dashboard.jsx

--- src/page/dashboard/Dashboard.jsx
+++ src/page/dashboard/Dashboard.jsx
@@ -12,11 +12,7 @@ const Dashboard = () => {
   return (
     <div>
 <Stack direction={"row"} justifyContent={"space-between"} alignItems={"center"}>
-        <Header
-          isDashboard={true}
-          title={"DASHBOARD"}
-          subTitle={"Welcome to your dashboard"}
-        />
+     
   
         <Box sx={{ textAlign: "right", mb: 1.3 }}>
           <Button

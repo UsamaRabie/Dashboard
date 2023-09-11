476b589578c12450fc0f447f4386a6139cabd997
Usama Rabie <76918546+UsamaRabie@users.noreply.github.com>
2023-09-12 00:59:16 +0300

    first commit

---
+++ src/page/form/Form.jsx
@@ -0,0 +1,189 @@
+import React, { useRef, useState } from "react";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import { Alert, Button, MenuItem, Snackbar, Stack } from "@mui/material";
+import { useForm } from "react-hook-form";
+import Header from "../../components/Header";
+
+const regEmail =
+  /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+const phoneRegExp =
+  /^((\+[1-9]{1,4}[ -]?)|(\([0-9]{2,3}\)[ -]?)|([0-9]{2,4})[ -]?)*?[0-9]{3,4}[ -]?[0-9]{3,4}$/;
+
+const data = [
+  {
+    value: "Admin",
+    label: "Admin",
+  },
+  {
+    value: "Manger",
+    label: "Manger",
+  },
+  {
+    value: "User",
+    label: "User",
+  },
+];
+
+const Form = () => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+
+  const [open, setOpen] = React.useState(false);
+
+  const handleClose = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+
+    setOpen(false);
+  };
+
+  const handleClick = () => {
+    setOpen(true);
+  };
+
+  const onSubmit = () => {
+    console.log("done! let's create new user");
+
+    handleClick();
+    form.current.reset()
+  
+  };
+  const[firstName , setFirstName]=useState("")
+  const[lastName , setLastName]=useState("")
+  const[email , setEmail]=useState("")
+  const[phone , setPhone]=useState("")
+  const[address1 , setAddress1]=useState("")
+  const[address2 , setAddress2]=useState("")
+
+  const form = useRef(null);
+  return (
+
+
+
+
+<Box>
+  
+  
+      <Header title="CREATE USER" subTitle="Create a New User Profile" />
+  
+      <Box
+      ref={form}
+        onSubmit={handleSubmit(onSubmit)}
+        component="form"
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          gap: 3,
+        }}
+        noValidate
+        autoComplete="off"
+      >
+        <Stack sx={{ gap: 2 }} direction={"row"}>
+          
+        <TextField
+              onChange={(e)=>setFirstName(e.target.value)}
+            error={Boolean(errors.firstName)}
+            helperText={
+              Boolean(errors.firstName)
+                ? "This field is required & min 3 character"
+                : null
+            }
+            {...register("firstName", { required: true, minLength: 3 })}
+            sx={{ flex: 1 }}
+            label="first Name"
+            variant="filled"
+          />
+  
+          <TextField
+              onChange={(e)=>setLastName(e.target.value)}
+            error={Boolean(errors.lastName)}
+            helperText={
+              Boolean(errors.lastName)
+                ? "This field is required & min 3 character"
+                : null
+            }
+            {...register("lastName", { required: true, minLength: 3 })}
+            sx={{ flex: 1 }}
+            label="Last Name"
+            variant="filled"
+          />
+        </Stack>
+  
+        <TextField
+       onChange={(e)=>setEmail(e.target.value)}
+          error={Boolean(errors.email)}
+          helperText={
+            Boolean(errors.email) ? "Please provide a valid email address" : null
+          }
+          {...register("email", { required: true, pattern: regEmail })}
+          label="Email"
+          variant="filled"
+        />
+  
+        <TextField
+          onChange={(e)=>setPhone(e.target.value)}
+          error={Boolean(errors.ContactNumber)}
+          helperText={
+            Boolean(errors.ContactNumber)
+              ? "Please provide a valid Phone number"
+              : null
+          }
+          {...register("ContactNumber", { required: true, pattern: phoneRegExp })}
+          label="Contact Number"
+          variant="filled"
+        />
+        <TextField onChange={(e)=>setAddress1(e.target.value)}   label="Adress 1" variant="filled" />
+        <TextField onChange={(e)=>setAddress2(e.target.value)}   label="Adress 2" variant="filled" />
+  
+        <TextField
+          variant="filled"
+          id="outlined-select-currency"
+          select
+          label="Role"
+          defaultValue="User"
+        >
+          {data.map((option) => (
+            <MenuItem key={option.value} value={option.value}>
+              {option.label}
+            </MenuItem>
+          ))}
+        </TextField>
+  
+        <Box sx={{ textAlign: "right" }}>
+          <Button
+            type="submit"
+            sx={{ textTransform: "capitalize" }}
+            variant="contained"
+          >
+            Create New User
+          </Button>
+  
+          <Snackbar
+            anchorOrigin={{ vertical: "top", horizontal: "right" }}
+            open={open}
+            autoHideDuration={3000}
+            onClose={handleClose}
+          >
+            <Alert onClose={handleClose} severity="info" sx={{ width: "100%" }}>
+              Account created successfully
+            </Alert>
+          </Snackbar>
+        </Box>
+      </Box>
+  
+  
+</Box>
+
+
+
+);
+};
+
+export default Form;

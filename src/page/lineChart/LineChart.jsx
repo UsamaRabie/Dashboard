5293d2eca82b80808d4df5731f57e48cb2f4afb0
Usama Rabie <76918546+UsamaRabie@users.noreply.github.com>
2023-09-12 01:41:16 +0300

    Update LineChart.jsx

--- src/page/lineChart/LineChart.jsx
+++ src/page/lineChart/LineChart.jsx
@@ -1,15 +1,14 @@
-import { Box, useTheme } from "@mui/material";
+import { Box} from "@mui/material";
 import React from "react";
 import { ResponsiveLine } from "@nivo/line";
 import Line from "./Line";
-import Header from "../../components/Header";
+
 
 const LineChart = () => {
-  const theme = useTheme();
+ 
   return (
     <Box>
-      <Header title="Line Chart" subTitle="Simple Line Chart" />
-
+      
       <Line />
     </Box>
   );

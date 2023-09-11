adbc80284d31b62ae46d8cf4e5b1363740475a4b
Usama Rabie <76918546+UsamaRabie@users.noreply.github.com>
2023-09-12 01:43:35 +0300

    Update Contacts.jsx

--- src/page/contacts/Contacts.jsx
+++ src/page/contacts/Contacts.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { DataGrid, GridToolbar } from "@mui/x-data-grid";
 import { Box, Typography } from "@mui/material";
 import { columns, rows } from "./data";
-import Header from "../../components/Header";
+
 
 const Contacts = () => {
   return (

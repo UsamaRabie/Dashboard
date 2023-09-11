d3c209620c7cdc3bf7488a3781c36a8f4a5ace86
Usama Rabie <76918546+UsamaRabie@users.noreply.github.com>
2023-09-12 01:45:30 +0300

    Update PieChart.jsx

--- src/page/pieChart/PieChart.jsx
+++ src/page/pieChart/PieChart.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { ResponsivePie } from "@nivo/pie";
 import { Box, useTheme } from "@mui/material";
 import Pie from "./pie";
-import Header from "../../components/Header";
+
 
 const PieChart = () => {
   const theme = useTheme();

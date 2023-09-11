93b8ee9741b8f166a761e0a594572a61e86566ce
Usama Rabie <76918546+UsamaRabie@users.noreply.github.com>
2023-09-12 01:43:07 +0300

    Update BarChart.jsx

--- src/page/barChart/BarChart.jsx
+++ src/page/barChart/BarChart.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { ResponsiveBar } from "@nivo/bar";
-import { Box, useTheme } from "@mui/material";
+import { Box } from "@mui/material";
 import Bar from "./bar";
-import Header from "../../components/Header";
+
 
 const BarChart = () => {
-  const theme = useTheme();
+
   return (
     <Box>
      
